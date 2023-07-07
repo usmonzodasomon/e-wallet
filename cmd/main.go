33c542289c765d17929726d0833fe8c39e9fba76
usmonzodasomon <budushiyprogrammist@gmail.com>
2023-07-07 18:11:46 +0500

    added gorm_db

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+	"github.com/usmonzodasomon/e-wallet/db"
+)
+
+func main() {
+	if err := initConfig(); err != nil {
+		log.Println("Error occured while init viper config: ", err)
+		return
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Printf("error loading env variables: %s", err)
+		return
+	}
+
+	var dbConfig db.Config
+	if err := viper.UnmarshalKey("db", &dbConfig); err != nil {
+		log.Println("Error unmarshalling config: ", err)
+		return
+	}
+	dbConfig.Password = os.Getenv("DB_PASSWORD")
+
+	db.StartDbConnection(dbConfig)
+
+	if err := db.Migrate(db.GetDBConn()); err != nil {
+		log.Println("Error while migrating tables: ", err)
+		return
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- models/user.go
+++ models/user.go
@@ -1,9 +1,9 @@
 package models
 
 type User struct {
-	ID       int64  `json:"id,omitempty"`
+	ID       int64  `json:"id,omitempty" gorm:"primaryKey"`
 	FIO      string `json:"fio,omitempty"`
 	Age      int    `json:"age,omitempty"`
-	Phone    string `json:"phone,omitempty"`
-	Password string `json:"password,omitempty"`
+	Phone    string `json:"phone,omitempty" gorm:"not null, unique"`
+	Password string `json:"password,omitempty" gorm:"not null"`
 }
